30881770077712c9c20728bd85ff00360a24d65d
Slimane Bendelloul <slimane.bendelloul@polymtl.ca>
2016-02-14 17:50:53 -0500

    Still with a bug

--- TP1/arborescence.py
+++ TP1/arborescence.py
@@ -3,28 +3,11 @@
 import sys
 import getopt
 import re
+import Queue
+import time
 from smallestenclosingcircle import make_circle
 
-def main(argv = None):
-    #input = sys.stdin.read()
-    input = "([(30,0),(10,10),(20,20),(30,40),(50,40)],200,1)"
 
-    list1 = input.split("]")
-    list2 = list1[1].strip(")").split(",");
-    K = list2[1]
-    C = list2[2]
-
-    list1 = list1[0].split("[")
-    list1 = list1[1]
-    string = ''.join(list1)
-    Positions = []
-
-    for i in re.findall(r"\(((?:'[^']*'|[^()])*)\)", string):
-        Positions.append(i.split(","))
-
-
-
-    search(Positions, K, C)
 
 
 class Antenne:
@@ -49,30 +32,71 @@ class Antenne:
 
 
 class Etat:
-    voisins = []
+    neighbours = []
     'Notre abstraction d\'un Etat'
     def __init__(self, antennes, ptsRestants):
         self.antennes = antennes
         self.ptsRestants = ptsRestants
 
+    def __str__(self):
+        ret = "Antennes :\n"
+        ret += str(self.antennes[1])
+
+        return ret
+
     def coutTotal(self, K, C):
         return sum([antenne.cout(K, C) for antenne in self.antennes])
+
 
     def etatsVoisins(self):
 
         # les etats voisin seront a une difference de l'etat orginale
 
         #on enleve un point de chaque antenne
-        k = 0
-        for position in self.antennes.positions:
-            # We need to take out a point from the antenna and make a new state with it
-            ptsSansPos = self.antennes.positions[:k] + self.antennes.positions[(k + 1):]
-            #print ptsSansPos
-            nvCercle = make_circle(ptsSansPos) # O(n) en complexite
-            nvAntenne = Antenne(nvCercle, ptsSansPos) #antenne avec les points couverts
+        antIndex = 0
 
-            print nvAntenne
-            k += 1
+        for antenne in self.antennes:
+            noAntenne = self.antennes[:antIndex] + self.antennes[(antIndex + 1):]
+            posIndex = 0
+            for position in antenne.positions:
+                # We need to take out a point from the antenna and make a new state with it
+                ptsSansPos = antenne.positions[:posIndex] + antenne.positions[(posIndex + 1):]
+                #print ptsSansPos
+                nvCercle = make_circle(ptsSansPos) # O(n) en complexite
+                nvAntenne = Antenne(nvCercle, ptsSansPos) #antenne avec les points couverts
+
+                #print nvAntenne
+
+                # We now creates neighbouring states with the new antennas
+                etat = Etat([noAntenne, nvAntenne], position)
+                self.neighbours.append(etat)
+                posIndex += 1
+
+            antIndex += 1
+
+            # We add a new point to the antenna
+            ptIndex = 0
+            for ptRestant in self.ptsRestants:
+                nvPtsRestants = self.ptsRestants[:ptIndex] + self.ptsRestants[(ptIndex + 1):] #nouvelles listes des points
+                nvPositions = [antenne.positions, ptRestant] #new antenna with an unsigned point
+                nvCercle = make_circle(nvPositions)
+                nvAntenne = Antenne(nvCercle, nvPositions)
+
+                print nvAntenne
+
+                etat = Etat([noAntenne, nvAntenne], nvPtsRestants)
+                self.neighbours.append(etat)
+                ptIndex += 1
+
+
+        #new Atennas with remaining pts
+        ptIndex = 0
+        for ptRestant in self.ptsRestants:
+            nvPtsRestants = self.ptsRestants[:ptIndex] + self.ptsRestants[(ptIndex + 1):] #nouvelles listes des points
+            nvAntenne = Antenne(make_circle(ptRestant), [ptRestant])
+            etat = Etat([self.antennes, nvAntenne], nvPtsRestants)
+            self.neighbours.append(etat)
+            ptIndex += 1
 
 
 
@@ -93,13 +117,48 @@ def search(Positions, K, C):
     #print posAntInitiale
 
     a = Antenne(posAntInitiale, Positions)
-    etatInitial = Etat(a, [])
+    etatInitial = Etat([a], [])
 
-    etatInitial.etatsVoisins()
+    sortedQueue = Queue.PriorityQueue()
+    sortedQueue.put_nowait((etatInitial.coutTotal(K, C), etatInitial))
 
-    #print a.cout(K, C)
+
+    #etatInitial.etatsVoisins()
+
+    while sortedQueue:
+		state = sortedQueue.get_nowait()[1]
+
+		if not state.unassigned:
+			return [a.info() for a in state.antennas]
+		else:
+			state.etatsVoisins()
+			for c in state.children:
+				sortedQueue.put_nowait((c.cost(K,C), c))
 
 
+    print etatInitial.neighbours[0]
+
+
+def main(argv = None):
+    #input = sys.stdin.read()
+    input = "([(30,0),(10,10),(20,20),(30,40),(50,40)],200,1)"
+
+    list1 = input.split("]")
+    list2 = list1[1].strip(")").split(",");
+    K = list2[1]
+    C = list2[2]
+
+    list1 = list1[0].split("[")
+    list1 = list1[1]
+    string = ''.join(list1)
+    Positions = []
+
+    for i in re.findall(r"\(((?:'[^']*'|[^()])*)\)", string):
+        Positions.append(i.split(","))
+
+
+
+    search(Positions, K, C)
 
 if __name__ == "__main__":
     main()
