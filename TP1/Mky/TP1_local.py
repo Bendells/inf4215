6c7903b1e58816174f3f2e11d6891bc89e2dd692
Mai-Khanh LE <lemaikhanh.lmk@gmail.com>
2016-02-14 23:02:36 -0500

    travail MK

---
+++ TP1/Mky/TP1_local.py
@@ -0,0 +1,135 @@
+from csp_depthfirst_search import *
+from hillclimbing import *
+from beam_search import *
+from state import *
+from node import *
+import time
+
+class AntennaState(State):
+    def __init__(self, Positions, K, C):
+        self.positions = Positions
+        self.K = K
+        self.C = C
+        self.dimensions = self._getSizeGrid(Positions)
+        self.grid = self._initializeGrid(self.dimensions)
+        self.antennaRay = 1
+        self.counter = 0
+        self.antennas = []
+        # initialisation des contraintes
+        self.constraints = [[0 for y in range(len(self.grid[0]))] for x in range(len(self.grid))]
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                self.constraints[i][j] = str('self.grid[{0}][{1}] == \'*\' or type(self.grid[{0}][{1}]) is int'.format(i,j))
+                
+    def equals(self,state):
+        return self.grid == state.grid
+
+    def show(self):
+        for row in self.grid:
+            for cell in row:
+                print '{:2}'.format(cell),
+            print
+
+    def executeAction(self, action):
+        self.counter += 1
+        (dimX, dimY) = self.dimensions
+        (x,y) = action[3]
+        for i in range(dimX):
+            for j in range(dimY):
+                position = (i,j)
+                positionAntenna = (action[0], action[1])
+                distance = self.getDistance(position, positionAntenna)
+                if (distance <= action[2] and not self.isSuperposing(position)):
+                    self.grid[i][j] = self.counter
+                    self.constraints[x][y] = str('type(self.grid[{0}][{1}]) is int'.format(x,y))
+        self.positions.remove(action[3])
+        self.antennas.append((action[0], action[1], action[2]))
+
+    def possibleActions(self):
+        actions = []
+        (dimX, dimY) = self.dimensions
+        for i in range(dimX):
+            for j in range(dimY):
+                positionAntenna = (i,j)
+                for position in self.positions:
+                    distance = self.getDistance(position, positionAntenna)
+                    self.antennaRay = self.findAntennaRay(positionAntenna)
+                    if distance <= self.antennaRay and not self.isSuperposing(positionAntenna):
+                        actions.append((i, j, self.antennaRay, position))
+        return actions
+
+    def findAntennaRay(self, positionAntenna):
+        antennaRay = 1
+        trouve = False
+        while not trouve:
+            for position in self.positions:
+                distance = self.getDistance(position, positionAntenna)
+                if distance <= antennaRay:
+                    trouve = True
+            antennaRay += 1
+        return antennaRay
+
+    def isSuperposing(self, position):
+        (dimX, dimY) = self.dimensions
+        for i in range(dimX):
+            for j in range(dimY):
+                distance = self.getDistance((i,j), position)
+                if distance <= self.antennaRay and self.grid[i][j] != '*':
+                    return True
+        return False
+
+    def getDistance(self, position, positionAntenna):
+        distance = math.sqrt(math.pow(position[0] - positionAntenna[0], 2) + math.pow(position[1] - positionAntenna[1], 2))
+        return distance
+            
+    def cost(self,action):
+        cost = self.K + self.C * math.pow(action[2], 2)
+        return cost
+    
+    def isGoal(self):
+        if len(self.positions) == 0:
+            for antenna in self.antennas:
+                print antenna
+            return True
+        else:
+            return False
+
+    def heuristic(self):
+        return self.K + self.C * math.pow(self.antennaRay, 2) / (self.counter + 1)
+ 
+    def consistent(self):
+        for row in self.constraints:
+            for c in row:
+                if not eval(c):
+                    return False
+        return True
+
+    def _initializeGrid(self, dimensions):
+        (dimX,dimY) = dimensions
+        grid = [[0 for y in range(dimY)] for x in range(dimX)]
+        for i in range(dimX):
+            for j in range(dimY):
+                grid[i][j] = '*'
+        return grid
+
+    def _getSizeGrid(self, Positions):
+        (x,y) = (-1, -1)
+        for position in Positions:
+            if position[0] > x:
+                x = position[0]
+            if position[1] > y:
+                y = position[1]
+        return (x+1,y+1)
+
+def search(Positions, K, C):
+    solution = hillclimbing_search(AntennaState(Positions, K, C))
+
+Positions = [(30,0),(10,10),(20,20),(30,40),(50,40)]
+K = 200
+C = 1
+
+## Lancement ##
+start = time.time()
+search(Positions, K, C)
+end = time.time()
+print '{} seconds'.format(end-start)
