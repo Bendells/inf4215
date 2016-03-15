abbdf824dd06a6964eef0fa589d9c437e2c97350
Slimane Bendelloul <slimane@eraid.biz>
2016-03-15 17:42:58 -0400

    Ajout du TP2 so far

---
+++ TP2/TP2-INF4215.pl
@@ -0,0 +1,96 @@
+
+	cours(mth1101).
+	cours(mth1006).
+	cours(mth1102).
+	cours(mth1110).
+	cours(mth1210).
+	cours(mth2302).
+	cours(inf1005).
+	cours(inf1010).
+	cours(inf2010).
+	cours(log2410).
+	cours(log2810).
+	cours(inf1040).
+	cours(inf1500).
+	cours(log1000).
+	cours(inf1600).
+	cours(inf2610).
+	cours(inf3710).
+	cours(inf1995).
+	cours(inf2990).
+	cours(log2420).
+	cours(ssh5100).
+	cours(inf3405).
+	cours(phs1101).
+	cours(ssh5201).
+	cours(inf4705).
+	cours(ssh5501).
+	cours(inf2705).
+	cours(inf4215).
+	cours(inf3500).
+	cours(ele2302).
+	cours(inf3610).
+	cours(log3000).
+	cours(inf3990).
+	cours(log3900).
+	cours(inf4990).
+	cours(log4900).
+	cours(inf3005).
+	cours(log3005).
+
+	prerequis(mth1102, mth1101).
+	prerequis(mth1110, mth1101).
+	prerequis(mth1110, mth1006).
+	prerequis(inf1010, inf1005).
+	prerequis(inf2010, inf1010).
+	prerequis(log2410, log1000).
+	prerequis(log2410, inf1010).
+	prerequis(log1000, inf1005).
+	prerequis(inf1600, inf1005).
+	prerequis(inf1600, inf1500).
+	prerequis(inf2610, inf1010).
+	prerequis(inf2610, inf1600).
+	prerequis(inf3710, inf2010).
+	prerequis(inf1995, inf1040).
+	prerequis(inf2990, inf2010).
+	prerequis(inf2990, log2410).
+	prerequis(inf3405, mth2302).
+	prerequis(inf4705, inf2010).
+	prerequis(inf4705, log2810).
+	prerequis(inf2705, mth1006).
+	prerequis(inf2705, inf2010).
+	prerequis(inf4215, log2810).
+	prerequis(inf4215, mth2302).
+	prerequis(inf3500, inf1600).
+	prerequis(ele2302, mth1110).
+	prerequis(inf3610, inf2610).
+	prerequis(inf3610, inf3500).
+	prerequis(log3000, inf2990).
+	prerequis(inf3990, inf3405).
+	prerequis(inf3990, inf3500).
+	prerequis(log3900, inf2990).
+	prerequis(inf4990, inf3990).
+	prerequis(log4900, log3900).
+
+	corequis(mth1102, mth1006).
+	corequis(mth1210, mth1110).
+	corequis(mth2302, mth1101).
+	corequis(inf2010, log2810).
+	corequis(inf3710, inf2610).
+	corequis(inf1995, inf1600).
+	corequis(inf1995, log1000).
+	corequis(log2420, inf2010).
+	corequis(inf2705, inf2990).
+	corequis(inf3500, ele2302).
+	corequis(inf3610, inf3990).
+	corequis(log3000, log3900).
+	corequis(inf3990, inf3005).
+	corequis(log3900, log3005).
+	
+	
+
+
+
+
+
+
